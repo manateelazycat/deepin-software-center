3969c035951585e0ba1984f01ca05924a2b24414
Andy Stewart <lazycat.manatee@gmail.com>
2011-06-18 07:59:31 +0800

    Update.

--- src/tools/findMissIcon.sh
+++ src/tools/findMissIcon.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-diff ../pkgInfo ../AppIcon | grep ../pkgInfo/
+for pkgfile in `ls ../pkgInfo/`
+do
+    if [ ! -f ../AppIcon/$pkgfile.png ]
+    then
+        echo $pkgfile
+    fi
+done
+
+# You can find icon through "http://ubuntu.allmyapps.com/apps/install-" + pkgfile
--- src/tools/findUnnecessaryIcon.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-diff ../pkgInfo/ ../AppIcon/ | grep ../AppIcon/

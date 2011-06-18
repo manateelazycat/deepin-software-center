0853b16821d37743b089e78667ee2295c8fb2cb4
Andy Stewart <lazycat.manatee@gmail.com>
2011-06-18 10:12:43 +0800

    Update icons.

--- src/tools/findMissIcon.sh
+++ src/tools/findMissIcon.sh
@@ -8,4 +8,8 @@ do
     fi
 done
 
-# You can find icon through "http://ubuntu.allmyapps.com/apps/install-" + pkgfile
+# You can find icon through:
+# "http://ubuntu.allmyapps.com/apps/install-" + pkgfile
+# "https://admin.fedoraproject.org/pkgdb/apps/search/" + pkgfile
+# "http://images.google.com/search?hl=zh-CN&gbv=2&biw=1366&bih=609&tbs=isz%3Aex%2Ciszw%3A48%2Ciszh%3A48&tbm=isch&sa=1&oq=grdesktop&aq=f&aqi=&aql=&gs_sm=e&gs_upl=23750l23750l0l1l1l0l0l0l0l0l0ll0&q=" + pkgfile
+

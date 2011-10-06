a5837a69ccb5c14fc5dcbfea1015ad2bd03e3b4e
Andy Stewart <lazycat.manatee@gmail.com>
2011-10-06 19:55:35 +0800

    Update.

--- src/downloadPage.py
+++ src/downloadPage.py
@@ -20,6 +20,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from appItem import *
 import gtk
 import pygtk
 pygtk.require('2.0')
@@ -34,6 +35,47 @@ class DownloadPage:
         self.view = gtk.TextView()
         self.view.get_buffer().set_text('下载管理')
         
+        self.topbar = Topbar()
+        
         # Connect components.
-        self.box.add(self.view)
+        self.box.pack_start(self.topbar.eventbox, False, False)
+        self.box.pack_start(self.view)
         self.box.show_all()
+
+class Topbar:
+    '''Top bar.'''
+	
+    def __init__(self):
+        '''Init for top bar.'''
+        # Init.
+        self.paddingX = 5
+        self.numColor = '#006efe'
+        self.normalColor = '#1A3E88'
+        self.hoverColor = '#0084FF'
+        self.selectColor = '#000000'
+        
+        self.box = gtk.HBox()
+        self.boxAlign = gtk.Alignment()
+        self.boxAlign.set(0.0, 0.5, 1.0, 1.0)
+        self.boxAlign.set_padding(0, 0, TOPBAR_PADDING_LEFT, TOPBAR_PADDING_UPDATE_RIGHT)
+        self.boxAlign.add(self.box)
+        self.eventbox = gtk.EventBox()
+        drawTopbar(self.eventbox)
+        
+        self.numLabel = gtk.Label()
+        
+        (self.openDirLabel, self.openDirEventBox) = utils.setDefaultClickableLabel("打开下载目录")
+        self.openDirEventBox.connect("button-press-event", lambda w, e: utils.runCommand("xdg-open /var/cache/apt/archives/"))
+        
+        # Connect.
+        self.numLabel.set_alignment(0.0, 0.5)
+        self.box.pack_start(self.numLabel, False, False, self.paddingX)
+        self.box.pack_start(self.openDirEventBox, False, False, self.paddingX)
+        self.eventbox.add(self.boxAlign)
+        
+    def updateNum(self, upgradeNum):
+        '''Update number.'''
+        self.numLabel.set_markup(
+            ("<span size='%s'>有 </span>" % (LABEL_FONT_SIZE))
+            + ("<span foreground='%s' size='%s'>%s</span>" % (self.numColor, LABEL_FONT_SIZE, str(upgradeNum)))
+            + ("<span size='%s'> 个更新包正在下载</span>" % (LABEL_FONT_SIZE)))

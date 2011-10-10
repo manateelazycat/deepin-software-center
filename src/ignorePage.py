b7e18efe814256a5e6ed892644234539f6fd3276
Andy Stewart <lazycat.manatee@gmail.com>
2011-10-10 23:30:34 +0800

    ignorePage.py: Set label's markup with blank if nothing to ignore.

--- src/ignorePage.py
+++ src/ignorePage.py
@@ -143,8 +143,11 @@ class Topbar:
     
     def updateNum(self, upgradeNum):
         '''Update number.'''
-        self.numLabel.set_markup(
-            ("<span size='%s'>有 </span>" % (LABEL_FONT_SIZE))
-            + ("<span foreground='%s' size='%s'>%s</span>" % (self.numColor, LABEL_FONT_SIZE, str(upgradeNum)))
-            + ("<span size='%s'> 款软件不再提醒升级</span>" % (LABEL_FONT_SIZE)))
+        # Don't show label if nothing to ignore.
+        if upgradeNum == 0:
+            markup = ""
+        else:
+            markup = ("<span size='%s'>有 </span>" % (LABEL_FONT_SIZE)) + ("<span foreground='%s' size='%s'>%s</span>" % (self.numColor, LABEL_FONT_SIZE, str(upgradeNum))) + ("<span size='%s'> 款软件不再提醒升级</span>" % (LABEL_FONT_SIZE))
+            
+        self.numLabel.set_markup(markup)
 

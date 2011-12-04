a34d7fbf0c7d93f4a59721d0e22026a6a4e07042
Andy Stewart <lazycat.manatee@gmail.com>
2011-12-05 00:18:25 +0800

    Add revert command.

--- repos.sh
+++ repos.sh
@@ -10,10 +10,14 @@ case "$1" in
     "push" )
         git push git@github.com:manateelazycat/deepin-software-center.git
         ;;
+    "revert" )
+        git checkout -- .
+        ;;
     * ) 
         echo "Help"
         echo "./repos.sh record         => record patch"
         echo "./repos.sh pull           => pull patch"
         echo "./repos.sh push           => push patch"
+        echo "./repos.sh revert         => revert patch"
         ;;
     esac

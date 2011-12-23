1ebe452c28bcf6cca20ca4149694002af2186503
Andy Stewart <lazycat.manatee@gmail.com>
2011-12-23 10:26:20 +0800

    Update repos.sh

--- repos.sh
+++ repos.sh
@@ -10,14 +10,22 @@ case "$1" in
     "push" )
         git push git@github.com:manateelazycat/deepin-software-center.git
         ;;
-    "revert" )
+    "changelog" )
+        git log --oneline
+        ;;
+    "checkout" )
         git checkout -- .
+        ;;
+    "revert" )
+        git revert $2
         ;;
     * ) 
         echo "Help"
         echo "./repos.sh record         => record patch"
         echo "./repos.sh pull           => pull patch"
         echo "./repos.sh push           => push patch"
-        echo "./repos.sh revert         => revert patch"
+        echo "./repos.sh changelog      => show changelog"
+        echo "./repos.sh checkout       => revert change code"
+        echo "./repos.sh revert         => revert patch by id"
         ;;
     esac
